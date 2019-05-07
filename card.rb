8160dce11ea3d576ea12278ce1b2e9f3a6bfa3e0
dmikhr <dmitrypkh@gmail.com>
2019-05-07 23:31:33 +0300

    Changing Card and Deck

--- card.rb
+++ card.rb
@@ -1,10 +1,31 @@
 class Card
-  attr_reader :name, :pic, :value
+  attr_reader :name
 
-  def initialize(suit, name, pic, value)
+  SUITS = { clubs: "\u2663", diamonds: "\u2666", hearts: "\u2665", spades: "\u2660" }.freeze
+  NAMES = { ace: 'Т', jack: 'В', queen: 'Д', king: 'К' }.freeze
+
+  def initialize(suit, name)
     @suit = suit
     @name = name
-    @pic = pic
-    @value = value
+  end
+
+  def pic
+    "#{card_symbol}#{SUITS[@suit]}"
+  end
+
+  def value
+    if @name.is_a?(Integer)
+      @name
+    elsif @name == :ace
+      11
+    else
+      10
+    end
+  end
+
+  private
+
+  def card_symbol
+    @name.is_a?(Integer) ? @name.to_s : NAMES[@name]
   end
 end
